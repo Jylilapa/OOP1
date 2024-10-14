974eb312c16bcd3532e7fb602c56a97af5502272
jylu lyapunova <jylilapa@yandex.ru>
2024-10-14 12:39:40 +0300

    bug fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,53 +6,53 @@ from src.product import Product
 
 @pytest.fixture
 def category_1():
-    return Category("Смартфоны",
-                    "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
-                    [
-      {
-        "name": "Samsung Galaxy C23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180000.0,
-        "quantity": 5
-      },
-      {
-        "name": "Iphone 15",
-        "description": "512GB, Gray space",
-        "price": 210000.0,
-        "quantity": 8
-      },
-      {
-        "name": "Xiaomi Redmi Note 11",
-        "description": "1024GB, Синий",
-        "price": 31000.0,
-        "quantity": 14
-      }
-    ])
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+        [
+            {
+                "name": "Samsung Galaxy C23 Ultra",
+                "description": "256GB, Серый цвет, 200MP камера",
+                "price": 180000.0,
+                "quantity": 5,
+            },
+            {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
+            {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14},
+        ],
+    )
 
 
 @pytest.fixture
 def category_2():
-    return Category("Телевизоры",
-                    "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                    [
-      {
-        "name": "55\" QLED 4K",
-        "description": "Фоновая подсветка",
-        "price": 123000.0,
-        "quantity": 7
-      }
-    ])
+    return Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [{"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}],
+    )
+
+
+@pytest.fixture
+def category_3():
+    return Category(
+        "Мониторы",
+        "Игровой монитор",
+        [
+            {"name": "Titan Army", "description": "Трехсторонний безрамочный дизайн", "price": 10499.0, "quantity": 8},
+            {
+                "name": "AOC",
+                "description": "Четкая смена кадров без их зависания и смазывания изображения.",
+                "price": 12499.0,
+                "quantity": 10,
+            },
+        ],
+    )
 
 
 @pytest.fixture
 def product_1():
-    return Product("Samsung Galaxy C23 Ultra",
-                   "256GB, Серый цвет, 200MP камера",
-                   180000.0, 5)
+    return Product("Samsung Galaxy C23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
 
 
 @pytest.fixture
 def product_2():
-    return Product("55\" QLED 4K",
-                   "Фоновая подсветка",
-                   123000.0, 7)
+    return Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,49 +1,47 @@
-
 def test_category_1(category_1):
     assert category_1.name == "Смартфоны"
-    assert (category_1.description ==
-            "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни")
+    assert (
+        category_1.description
+        == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
+    )
     assert category_1.products == [
-      {
-        "name": "Samsung Galaxy C23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180000.0,
-        "quantity": 5
-      },
-      {
-        "name": "Iphone 15",
-        "description": "512GB, Gray space",
-        "price": 210000.0,
-        "quantity": 8
-      },
-      {
-        "name": "Xiaomi Redmi Note 11",
-        "description": "1024GB, Синий",
-        "price": 31000.0,
-        "quantity": 14
-      }
+        {
+            "name": "Samsung Galaxy C23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        },
+        {"name": "Iphone 15", "description": "512GB, Gray space", "price": 210000.0, "quantity": 8},
+        {"name": "Xiaomi Redmi Note 11", "description": "1024GB, Синий", "price": 31000.0, "quantity": 14},
     ]
-
-
-def test_category_count_1(category_1):
-    assert category_1.count_category == 2
+    assert category_1.count_category == 1
     assert category_1.count_products == 3
 
 
 def test_category_2(category_2):
     assert category_2.name == "Телевизоры"
-    assert (category_2.description ==
-            "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником")
+    assert (
+        category_2.description
+        == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+    )
     assert category_2.products == [
-      {
-        "name": "55\" QLED 4K",
-        "description": "Фоновая подсветка",
-        "price": 123000.0,
-        "quantity": 7
-      }
+        {"name": '55" QLED 4K', "description": "Фоновая подсветка", "price": 123000.0, "quantity": 7}
     ]
-
-
-def test_category_count_2(category_2):
-    assert category_2.count_category == 4
+    assert category_2.count_category == 2
     assert category_2.count_products == 1
+
+
+def test_category_3(category_3):
+    assert category_3.name == "Мониторы"
+    assert category_3.description == "Игровой монитор"
+    assert category_3.products == [
+        {"name": "Titan Army", "description": "Трехсторонний безрамочный дизайн", "price": 10499.0, "quantity": 8},
+        {
+            "name": "AOC",
+            "description": "Четкая смена кадров без их зависания и смазывания изображения.",
+            "price": 12499.0,
+            "quantity": 10,
+        },
+    ]
+    assert category_3.count_category == 3
+    assert category_3.count_products == 2
